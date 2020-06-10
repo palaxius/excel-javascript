9a60eeb123f8a5495c4f3c1b1a55c5d8794b1544
Andrey Avtushenko <palaxius@mail.ru>
2020-06-10 18:36:23 +0300

    comment debug

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -12,10 +12,10 @@ export class Excel {
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      // debug
-      if (component.name) {
-        window['c' + component.name] = component
-      }
+      // // debug
+      // if (component.name) {
+      //   window['c' + component.name] = component
+      // }
 
       $el.html(component.toHTML())
       $root.append($el)
