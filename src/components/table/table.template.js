f2cb7f660d0d44b83827097312433da532e547ed
Andrey Avtushenko <palaxius@mail.ru>
2020-06-13 19:53:53 +0300

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,22 +3,29 @@ const CODES = {
   Z: 90
 }
 
-function toCell() {
+function toCell(_, col) {
   return `
-  <div class="cell" contenteditable></div>
+  <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
-function toColumn(col = '') {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+        ${col} 
+        <div class="col-resize" data-resize="col"></div>
+    </div>
   `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class="row"> 
-        <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable"> 
+        <div class="row-info">
+            ${index ? index : ''}     
+            ${resize}      
+        </div>
         <div class="row-data">${content}</div>
     </div>
   `
