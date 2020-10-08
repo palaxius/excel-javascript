3ea6ce6f79f2066f1c218082f8d24f6e0e01eb5d
Andrey Avtushenko <palaxius@mail.ru>
2020-10-08 12:40:21 +0300

    full commit

--- src/components/table/table.functions.js
+++ src/components/table/table.functions.js
@@ -13,6 +13,7 @@ export function matrix($target, $current) {
   const current = $current.id(true)
   const cols = range(current.col, target.col)
   const rows = range(current.row, target.row)
+
   return cols.reduce((acc, col) => {
     rows.forEach(row => acc.push(`${row}:${col}`))
     return acc
@@ -37,5 +38,6 @@ export function nextSelector(key, {col, row}) {
       row = row - 1 < MIN_VALUE ? MIN_VALUE : row - 1
       break
   }
+
   return `[data-id="${row}:${col}"]`
 }
